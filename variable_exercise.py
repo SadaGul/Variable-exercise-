83a2a6c74951e0c696d3b7c5c74ab39c3cb81d4a
SadaGul <154694339+SadaGul@users.noreply.github.com>
2024-02-27 23:30:02 +0500

    Add files via upload

---
+++ variable_exercise.py
@@ -0,0 +1,44 @@
+#variable exercise
+#Exercise no 1
+name = ("SadaGul")
+age = ("19")
+print(name)
+print(age)
+
+#Exercise no 2
+num1 = 4
+num2 = 6
+result = num1+num2
+print(result)
+
+#Exercise no 3
+price = 2.5
+quantity = 5
+total_cost =(price*quantity)
+print(total_cost)
+
+#Exercise no 4
+frist_name ="Sada"
+last_name = "Gul"
+ful_name = (frist_name+last_name)
+print(ful_name)
+
+#Exercise no 5
+is_adult = "true"
+print(is_adult)
+
+#Exercise no 6
+user_input = input(" enter your name:")
+print(f"Hello, {user_input} Welcome.")
+
+#Exercise no 7
+celsius_temperature = 25
+fahrenheit_temperature = (celsius_temperature * 9/5) + 32
+print(f"Original Celsius Temperature: {celsius_temperature}")
+print(f"Converted Fahrenheit Temperature: {fahrenheit_temperature}")
+
+#Exercise no 8
+a = 43
+b = 34
+a,b=b,a
+print(f"After swapping, a: {a}, b: {b}")
